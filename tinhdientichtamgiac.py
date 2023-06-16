be982e59ac7c777f5fd8b99683f073018b090d29
Nhii0492 <Nhinguyen0492@gmail.com>
2023-06-16 21:46:58 +0700

    Update tinhdientichtamgiac.py

--- tinhdientichtamgiac.py
+++ tinhdientichtamgiac.py
@@ -4,10 +4,9 @@ a=float(input("Nhap canh a: "))
 b=float(input("Nhap canh b: "))
 c=float(input("Nhap canh c: "))
 if a<=0 and b<=0 and c<=0:
-	print("Nhap lai ")
+    print("Nhap lai ")
 else:
-	p=(a+b+c)/2
-		print("chu vi cua tam giac la: ",p)
-	s= math.sqrt(p*(p-a)*(p-b)*(p-c))
-		print("dien tich cua tam giac la: ",s)
-
+    p=(a+b+c) / 2
+    s=(p*(p-a)*(p-b)*(p-c))**0.5
+    print("chu vi cua tam giac la: ",p)
+    print("dien tich cua tam giac la: ",s)
