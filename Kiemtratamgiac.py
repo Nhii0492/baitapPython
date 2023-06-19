e88eecca356399dcc683880adb3576ed87a85de0
Nhii0492 <Nhinguyen0492@gmail.com>
2023-06-19 19:59:22 +0700

    Update Kiemtratamgiac.py

--- Kiemtratamgiac.py
+++ Kiemtratamgiac.py
@@ -2,15 +2,17 @@ print('Nhap cac canh cua tam giac')
 a=float(input("nhap canh a: "))
 b=float(input("nhapcanh b: "))
 c=float(input("nhap canh c: "))
-print('kiem tra 3 canh co phai la tam giac')
-answer=(a + b > c) and (a + c > b) and (b + c > a) and (a!=0) and (b!=0) and (c!=0)
-print(answer)
-print('Kiem tra tam giac deu')
-answer = (a==b==c) and (a!=0) and (b!=0) and (c!=0)
-print(answer)
-print('kiem tra tam giac can')
-answer= ((a==b) or (a==c) or (b==c)) and ((a!=0) and (b!=0) and (c!=0))
-print(answer)
-print('Kiem tra tam giac vuong')
-answer = ((a*a == b*b + c*c) or (b*b == a*a + c*c) or (c*c == a*a + b*b))  and ((a!=0) and (b!=0) and (c!=0))
-print(answer)
+tg=(a + b > c) and (a + c > b) and (b + c > a) and (a!=0) and (b!=0) and (c!=0)
+print('kiem tra 3 canh co phai la tam giac: ',tg)
+
+tg_deu = ((a==b==c) and (a!=0) and (b!=0) and (c!=0)) and tg 
+print('tam giac deu: ',tg_deu)
+
+tg_can= ((a==b) or (a==c) or (b==c)) and ((a!=0) and (b!=0) and (c!=0)) and tg 
+print('tam giac can : ',tg_can)
+
+tg_vuong = tg and ((a*a == b*b + c*c) or (b*b == a*a + c*c) or (c*c == a*a + b*b))  and ((a!=0) and (b!=0) and (c!=0))
+print('tam giac vuong: ',tg_vuong)
+
+tg_thuong = tg and not tg_deu and not tg_vuong
+print('tam giac thuong: ',tg_thuong)
