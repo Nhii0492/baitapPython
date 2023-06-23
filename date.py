c55a4343d1ed73ded02b2e8652d38ce07ca7b5dd
Nhii0492 <Nhinguyen0492@gmail.com>
2023-06-23 22:16:12 +0700

    Create date.py

---
+++ date.py
@@ -0,0 +1,18 @@
+num=float(input('Nhap vao mot so: '))
+if num==1:
+    print('Hom nay la thu 2')
+elif num==2:
+    print('Hom nay la thu 3')
+elif num==3:
+    print('Hom nay la thu 4')
+elif num==4:
+    print('Hom nay la thu 5')
+elif num==5:
+    print('Hom nay la thu 6')
+elif num==6:
+    print('Hom nay la thu 7')
+elif num==7:
+    print('Hom nay la chu nhat')
+else:
+    print('Loi du lieu')
+
