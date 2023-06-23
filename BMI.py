7afd889e6bfb73281611d62e1de3ded3d9e98458
Nhii0492 <Nhinguyen0492@gmail.com>
2023-06-23 21:41:48 +0700

    Create BMI.py

---
+++ BMI.py
@@ -0,0 +1,23 @@
+print('BMI')
+chieu_cao=float(input('Nhap chieu cao: '))
+can_nang=float(input('Nhap can nang: '))
+if chieu_cao <0 and can_nang <0:
+    print('phai >0')
+else:
+    BMI= can_nang/(chieu_cao**2)
+    if BMI <16:
+        print('Gầy cấp độ III')
+    elif 16<= BMI <17:
+        print('Gầy cấp độ II')
+    elif 17<= BMI <18.5:
+        print('Gầy cấp độ I')
+    elif 18.5<= BMI <25:
+        print('Bình thường')
+    elif 25<= BMI <30:
+        print('Thừa cân')
+    elif 30<= BMI <35:
+        print('Béo phì cấp độ I')
+    elif 35<= BMI <40:
+        print('Béo phì cấp độ II')
+    elif BMI >40
+            print('Béo phì cấp độ Iii')
