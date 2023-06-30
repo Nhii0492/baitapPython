28938b138ba5d23efabda43ff166c25fe8244964
Nhii0492 <Nhinguyen0492@gmail.com>
2023-06-30 19:57:50 +0700

    Create bangcuuchuong.py

---
+++ bangcuuchuong.py
@@ -0,0 +1,10 @@
+try:
+    n = int(input('Bảng cửu chương cửu: '))
+    if n<1 or n>9
+        print("n phải năm từ 1 đến 9")
+    else:   
+        for i in range(1, 11):
+            result=n*i
+            print(n,"x",i,"=",result)
+except:
+    print("error")
