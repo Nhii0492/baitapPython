3c2b38a7d7c54a517f024050a0556f8640171041
Nhii0492 <Nhinguyen0492@gmail.com>
2023-06-30 21:22:18 +0700

    Update Fizz Buzz.py

--- Fizz Buzz.py
+++ Fizz Buzz.py
@@ -1,15 +1,23 @@
-string=input('Nhập 2 số bất kì cách nhau bằng dầu ",": ')
-so=string.split(",")
-start=int(so[0])
-end=int(so[1])
-print("start =",start)
-print("end=",end)
-for num in range(start, end + 1):   
-        if num % 3 == 0:
-            print("Fizz")
-        if num % 5 == 0:
-            print("Buzz")
-        elif num % 3 == 0 and num % 5 == 0:
-            print("FizzBuzz")
-        else:
-            print(num)
+try:
+    string=input('Nhập 2 số bất kì cách nhau bằng dầu ",": ')
+    so=string.split(",")
+    start=int(so[0].strip())
+    end=int(so[1].strip())
+    print("start =",start)
+    print("end=",end)
+    if end < start:
+        tmp = start
+        start=end
+        end=tmp
+    print(start,end)
+    for num in range(start, end + 1):   
+            if num % 3 == 0:
+                print("Fizz")
+            elif num % 5 == 0:
+                print("Buzz")
+            elif num % 3 == 0 and num % 5 == 0:
+                print("FizzBuzz")
+            else:
+                print(num)
+except:
+    print("error")
