858d379b150156a2205b5f8ac285310c691d07b9
Nhii0492 <Nhinguyen0492@gmail.com>
2023-07-07 15:48:37 +0700

    Update Fizz Buzz.py

--- Fizz Buzz.py
+++ Fizz Buzz.py
@@ -1,23 +1,24 @@
 try:
-    string=input('Nhập 2 số bất kì cách nhau bằng dầu ",": ')
-    so=string.split(",")
-    start=int(so[0].strip())
-    end=int(so[1].strip())
-    print("start =",start)
-    print("end=",end)
+    string = input('Nhập 2 số bất kỳ cách nhau bằng dấu phẩy (,): ')
+    so = string.split(",")
+    start = int(so[0].strip())
+    end = int(so[1].strip())
+    print("start =", start)
+    print("end =", end)
     if end < start:
-        tmp = start
-        start=end
-        end=tmp
-    print(start,end)
+        start, end = end, start
     for num in range(start, end + 1):   
-            if num % 3 == 0:
-                print("Fizz")
-            elif num % 5 == 0:
-                print("Buzz")
-            elif num % 3 == 0 and num % 5 == 0:
-                print("FizzBuzz")
-            else:
-                print(num)
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+except ValueError:
+    print("Lỗi! Vui lòng nhập 2 số nguyên cách nhau bằng dấu phẩy (,).")
+except IndexError:
+    print("Lỗi! Vui lòng nhập đúng định dạng 2 số cách nhau bằng dấu phẩy (,).")
 except:
-    print("error")
+    print("Lỗi không xác định!")
