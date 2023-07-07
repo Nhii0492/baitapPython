9adfdc567e20cb5d0174253134d024bd4d74236f
Nhii0492 <Nhinguyen0492@gmail.com>
2023-07-07 15:49:38 +0700

    Update bangcuuchuong.py

--- bangcuuchuong.py
+++ bangcuuchuong.py
@@ -1,10 +1,10 @@
 try:
     n = int(input('Bảng cửu chương cửu: '))
-    if n<1 or n>9
-        print("n phải năm từ 1 đến 9")
+    if n < 1 or n > 9:
+        print("n phải nằm trong khoảng từ 1 đến 9")
     else:   
         for i in range(1, 11):
-            result=n*i
-            print(n,"x",i,"=",result)
-except:
-    print("error")
+            result = n * i
+            print(n, "x", i, "=", result)
+except ValueError:
+    print("Lỗi! Vui lòng nhập một số nguyên.")
